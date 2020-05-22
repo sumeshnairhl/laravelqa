ed92385789bfb26c796483559f2b87ded236baf6
sumeshnairhl <sumeshnairhl@gmail.com>
2020-05-22 17:26:46 +0530

    questions listing

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('questions','QuestionsController');
+Route::resource('questions','QuestionController');
