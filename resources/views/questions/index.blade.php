022327386a490510d13abe4c710770927a0dd172
sumeshnairhl <sumeshnairhl@gmail.com>
2020-05-22 14:13:45 +0530

    questions listing with pagination

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">All Questions</div>
+
+                <div class="panel-body">
+                    @foreach($questions as $question)
+                        <div class="media">
+                            <div class="media-body">
+                                <h3>{{ $question->title}}</h3>
+                                {{ str_limit($question->body,250) }}
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="text-center">
+                        {{ $questions->links()}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
